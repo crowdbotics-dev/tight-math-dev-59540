5822f91376d6374dc7cbd5605c768c1402ebfc0e
Crowdbotics <engineering@crowdbotics.com>
2023-01-27 01:38:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-math-dev-59540",
+  url: "https://tight-math-dev-59540.botics.co",
+  api: "https://tight-math-dev-59540.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
